889c6e624917358674838ed0e4ca61312920f8d8
LoganCat <javier.gh81@gmail.com>
2024-03-21 09:11:13 -0600

    Detallar

--- script.js
+++ script.js
@@ -9,18 +9,18 @@ botonDesencriptar.onclick = desencriptar;
 
 function encriptar(){
     ocultarAdelante();
-    var cajatexto = recuperaTexto();
+    var cajatexto = recuperarTexto();
     textoEncriptarDesencriptar.textContent = encriptarTexto(cajatexto);
 }
 
 function desencriptar(){
     ocultarAdelante();
-    var cajatexto = recuperaTexto();
+    var cajatexto = recuperarTexto();
     textoEncriptarDesencriptar.textContent = desencriptarTexto(cajatexto);
 }
 
-function recuperaTexto(){
-    var cajatexto = document.querySelector(".cajatexto");
+function recuperarTexto(){
+    var cajatexto = document.querySelector(".texto-ingresado");
     return cajatexto.value;
 }
 
@@ -29,4 +29,69 @@ function ocultarAdelante(){
     h3yP.classList.add("ocultar");
 }
 
+function encriptarTexto(mensaje){
+    var texto = mensaje;
+    var textoFinal = "";
 
+    for(var i = 0; i < texto.length; i++){
+        if(texto[i] == "a"){
+            textoFinal = textoFinal + "ai"
+        }
+        else if(texto[i] == "e"){
+            textoFinal = textoFinal + "enter"
+        }
+        else if(texto[i] == "i"){
+            textoFinal = textoFinal + "imes"
+        }
+        else if(texto[i] == "o"){
+            textoFinal = textoFinal + "ober"
+        }
+        else if(texto[i] == "u"){
+            textoFinal = textoFinal + "ufat"
+        }
+        else{
+            textoFinal = textoFinal + texto[i]
+        }
+    }
+    return textoFinal;
+}
+
+function desencriptarTexto(mensaje){
+    var texto = mensaje;
+    var textoFinal = "";
+
+    for(var i = 0; i < texto.length; i++){
+        if(texto[i] == "a"){
+            textoFinal = textoFinal + "a"
+            i = i+1;
+        }
+        else if(texto[i] == "e"){
+            textoFinal = textoFinal + "e"
+            i = i+4;
+        }
+        else if(texto[i] == "i"){
+            textoFinal = textoFinal + "i"
+            i = i+3;
+        }
+        else if(texto[i] == "o"){
+            textoFinal = textoFinal + "o"
+            i = i+3;
+        }
+        else if(texto[i] == "u"){
+            textoFinal = textoFinal + "u"
+            i = i+3;
+        }
+        else{
+            textoFinal = textoFinal + texto[i]
+        }
+    }
+    return textoFinal;
+}
+
+const btnCopiar = document.querySelector(".copiar");
+    btnCopiar.addEventListener("click", copiar = () => {
+        var contenido = document.querySelector(".texto-desencriptado")
+        .textContent;
+        navigator.clipboard.writeText(contenido);
+        console.log("hola mundo");
+    });
